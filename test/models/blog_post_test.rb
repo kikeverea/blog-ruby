181339e5b242b8a0555fbc33ee1c11bfa056f731
Enrique Verea <kikeverea@gmail.com>
2024-03-27 07:15:32 +0100

    Add tests for BlogPost status methods

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,7 +1,47 @@
 require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  #### draft? ####
+
+  test "draft? returns true for BlogPosts with nil 'publised_at' date " do
+    # binding.irb
+    assert blog_posts(:draft).draft?
+  end
+
+  test "draft? returns false for BlogPosts with future 'publised_at' date " do
+    refute blog_posts(:scheduled).draft?
+  end
+
+  test "draft? returns false for BlogPosts with past 'publised_at' date " do
+    refute blog_posts(:published).draft?
+  end
+
+  #### scheduled? ####
+
+  test "scheduled? returns true for BlogPosts with future 'publised_at' date " do
+    assert blog_posts(:scheduled).scheduled?
+  end
+
+  test "scheduled? returns false for BlogPosts with past 'publised_at' date " do
+    refute blog_posts(:published).scheduled?
+  end
+
+  test "scheduled? returns false for BlogPosts with nil 'publised_at' date " do
+    refute blog_posts(:draft).scheduled?
+  end
+
+  #### published? ####
+
+  test "published? returns true for BlogPosts with past 'publised_at' date " do
+    assert blog_posts(:published).scheduled?
+  end
+
+  test "published? returns false for BlogPosts with future 'publised_at' date " do
+    refute blog_posts(:scheduled).scheduled?
+  end
+
+  test "published? returns false for BlogPosts with nil 'publised_at' date " do
+    refute blog_posts(:draft).scheduled?
+  end
 end
